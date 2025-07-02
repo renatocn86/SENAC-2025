56865431ad5496df4a51d1d44cfd0eeb836617a6
Renato Nascimento <96842858+renatocn86@users.noreply.github.com>
2025-07-01 22:08:30 -0300

    Pasta css na pages, DOM e card com js

---
+++ front-end/pages/javascript/dom.js
@@ -0,0 +1,16 @@
+//document.writeln('Hello World!')
+
+var container = document.getElementById('container') //pegando div do html
+var elemento = document.createElement('button') // criando um botão
+elemento.textContent = 'Button' // define o conteúdo textual do elemento
+// elemento.style.backgroundColor = '#ff0000' //é possível estilizar os elementos pelo JS
+elemento.id = 'btn' // define um id ao elemento
+elemento.className = 'btn' // define uma classe ao elemento, 
+// .classList também pode ser usado se for adicionar mais de uma classe
+
+container.appendChild(elemento) // adiciona um elemento filho 
+
+texto = 'Literal'
+classe = 'btn'
+container.innerHTML += `<button class="btn">Literal</button>` // Literal com valores pré-definidos
+container.innerHTML += `<button class="${classe}">${texto}</button>` // Literal com valores dinâmicos
