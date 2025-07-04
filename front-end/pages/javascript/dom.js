50e1b5532d04a3fe262cbe658e7274d513c4ce39
Renato Nascimento <96842858+renatocn86@users.noreply.github.com>
2025-07-03 21:05:49 -0300

    Múltiplos cards + imagens

--- front-end/pages/javascript/dom.js
+++ front-end/pages/javascript/dom.js
@@ -8,6 +8,10 @@ elemento.id = 'btn' // define um id ao elemento
 elemento.className = 'btn' // define uma classe ao elemento, 
 // .classList também pode ser usado se for adicionar mais de uma classe
 
+function Acao(){
+    alert("Ação")
+}
+
 container.appendChild(elemento) // adiciona um elemento filho 
 
 texto = 'Literal'
@@ -17,15 +21,46 @@ container.innerHTML += `<button class="${classe}">${texto}</button>` // Literal 
 
 var container2 = document.getElementById('container-2')
 
-container2.innerHTML = `
+var pessoa = [
+    {
+        "nome": "Lena",
+        "foto": "http://127.0.0.1:5500/front-end/images/lena.jpg",
+        "post": "http://127.0.0.1:5500/front-end/images/lena2.jpg",
+        "descricao": "Lena"
+    }, {
+        "nome": "Ada",
+        "foto": "http://127.0.0.1:5500/front-end/images/ada.jpg",
+        "post": "http://127.0.0.1:5500/front-end/images/ada2.jpg",
+        "descricao": "Ada Lovelace"
+    }, {
+        "nome": "Oscar",
+        "foto": "http://127.0.0.1:5500/front-end/images/oscar.jpg",
+        "post": "http://127.0.0.1:5500/front-end/images/oscar2.jpg",
+        "descricao": "Oscar Niemeyer"
+    }, {
+        "nome": "Alan",
+        "foto": "http://127.0.0.1:5500/front-end/images/alan.jpg",
+        "post": "http://127.0.0.1:5500/front-end/images/alan2.jpg",
+        "descricao": "Alan Turing"
+    }
+]
+
+for (let i = 0; i < pessoa.length; i++) {
+    container2.innerHTML += `
         <div class="card">
 
-            <div class="img"></div>
+            <img class="img" src="${pessoa[i].post}" 
+            width="300" height="200"></img>
             
             <div class="row">
-                <div class="avatar"></div>
-                <p>${texto}</p>
+                <img class="avatar" src="${pessoa[i].foto}"
+                width="50" height="50"></img>
+
+                <p>${pessoa[i].descricao}</p>
             </div>
 
         </div>
-`
+    `
+}
+
+
