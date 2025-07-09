b054a4e4e8896ea8449155395fe476c25f89a20a
Renato Nascimento <96842858+renatocn86@users.noreply.github.com>
2025-07-08 21:26:42 -0300

    Jogo finalizado

--- front-end/pages/javascript/jogo.js
+++ front-end/pages/javascript/jogo.js
@@ -1,6 +1,16 @@
 
 var container = document.getElementById('container')
-flag = true
+var gameContainer = document.createElement('div')
+gameContainer.className = 'game-container'
+
+container.appendChild(gameContainer)
+
+for(let i = 0; i < 25; i++){
+    let cell = document.createElement('div')
+    cell.className = 'cell'
+    gameContainer.appendChild(cell)
+}
+
 function CreateSquare(){
     if(document.querySelector('.square')!==null){
         document.querySelector('.square').remove()
@@ -10,13 +20,17 @@ function CreateSquare(){
     a.addEventListener('click', ()=>{
         CreateSquare()
     })
-    if(flag){
-        a.style.backgroundColor = "blue"
-    } else{
-        a.style.backgroundColor = "red"
-    }
-    flag = !flag
-    container.appendChild(a)
+
+    let red = Math.floor(Math.random()*256)
+    let green = Math.floor(Math.random()*256)
+    let blue = Math.floor(Math.random()*256)
+    a.style.backgroundColor = 'rgb( '+red+','+green+','+blue+')'
+
+    var cells = document.querySelectorAll('.cell')
+    var randomIndex = Math.floor(Math.random()*25) 
+    let randomCell = cells[randomIndex]
+
+    randomCell.appendChild(a)
 }
 
 CreateSquare()
--- front-end/scripts/json.js
+++ front-end/scripts/json.js
@@ -1,55 +1,57 @@
-//JSON - JavaScript Object Notation
+function Json() {
+    //JSON - JavaScript Object Notation
 
-var pessoa = { // JSON -> estrutura de dados para representar um objeto
-    "nome": "Lena",
-    "foto": "http://...",
-    "post": "http://...",
-    "descricao": "text"
-}
+    var pessoa = { // JSON -> estrutura de dados para representar um objeto
+        "nome": "Lena",
+        "foto": "http://...",
+        "post": "http://...",
+        "descricao": "text"
+    }
 
-console.log(pessoa)
+    console.log(pessoa)
 
-console.log(pessoa.nome) // Para acessar atributos do objeto, 
-// ultiliza-se o operador '.' seguido da chave. Ex: 'pessoa.nome', 
-// onde 'pessoa' é o objeto e 'nome' é o atributo.
+    console.log(pessoa.nome) // Para acessar atributos do objeto, 
+    // ultiliza-se o operador '.' seguido da chave. Ex: 'pessoa.nome', 
+    // onde 'pessoa' é o objeto e 'nome' é o atributo.
 
-pessoa.nome = "Luana" // Para editar o atributo de um objeto, acessamos 
-// o atributo e utilizamos o operador de atribuição '=' para definir o
-// o novo valor.
+    pessoa.nome = "Luana" // Para editar o atributo de um objeto, acessamos 
+    // o atributo e utilizamos o operador de atribuição '=' para definir o
+    // o novo valor.
 
-var produto = {
-    "nome": "celular 01",
-    "id": 0,
-    "descricao": "descrição",
-    "preco": "1000.00"
-}
-
-var produtos = [ // Lista de produtos
-    {
+    var produto = {
         "nome": "celular 01",
         "id": 0,
         "descricao": "descrição",
-        "preco": "2000.00"
-    },
-    {
-        "nome": "celular 02",
-        "id": 1,
-        "descricao": "descrição",
-        "preco": "1500.00"
-    },
-    {
-        "nome": "celular 03",
-        "id": 2,
-        "descricao": "descrição",
-        "preco": "1900.00"
-    },
-    {
-        "nome": "celular 04",
-        "id": 3,
-        "descricao": "descrição",
-        "preco": "3100.00"
+        "preco": "1000.00"
     }
-]
 
-console.log(produtos[2]) // Para acessar um ítem da lista 
-// utilizamos o index(lista[x])
+    var produtos = [ // Lista de produtos
+        {
+            "nome": "celular 01",
+            "id": 0,
+            "descricao": "descrição",
+            "preco": "2000.00"
+        },
+        {
+            "nome": "celular 02",
+            "id": 1,
+            "descricao": "descrição",
+            "preco": "1500.00"
+        },
+        {
+            "nome": "celular 03",
+            "id": 2,
+            "descricao": "descrição",
+            "preco": "1900.00"
+        },
+        {
+            "nome": "celular 04",
+            "id": 3,
+            "descricao": "descrição",
+            "preco": "3100.00"
+        }
+    ]
+
+    console.log(produtos[2]) // Para acessar um ítem da lista 
+    // utilizamos o index(lista[x])
+}
