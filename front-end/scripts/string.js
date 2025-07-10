d7139a9beed39ceaa7532516368c7b995aa62a9f
Renato Nascimento <96842858+renatocn86@users.noreply.github.com>
2025-07-09 22:06:09 -0300

    Form e string

---
+++ front-end/scripts/string.js
@@ -0,0 +1,20 @@
+var text = 'O rato roeu a roupa do rei de roma'
+var palavras = text.split('')
+for(let i = 0; i< palavras.length; i++){
+    //alert(palavras[i])
+    if("rato" === palavras[i]){
+        palavras[i] = 'cachorro'
+    }
+}
+//alert(palavras.join(' ').replace('roma', 'grécia'))
+
+// Faça um script que conte quantas vezes a 
+// letra 'o' (minúscula) aparece na frase.
+var soma = 0
+for(let i = 0; i < palavras.length;i++){
+    if("o" === palavras[i]){
+        soma++
+    }
+}
+alert(soma)
+alert(text)
