05fcfe2f45592358ef51cb028da779b1c4a93549
Renato Nascimento <96842858+renatocn86@users.noreply.github.com>
2025-07-11 18:21:36 -0300

    String + semântico

---
+++ front-end/pages/javascript/semantico.js
@@ -0,0 +1,12 @@
+
+var right = document.querySelector('.right')
+
+for(let i = 0;i< 10; i++){
+    right.innerHTML +=`
+            <div class="card">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKvz-cKC0Yw7ZQEzUSLGby_FrWt6od_-7oxQ&s" alt="" width="200" height="auto">
+                <h3>Titulo da imagem</h3>
+            </div>
+    `
+}
+
--- front-end/scripts/string.js
+++ front-end/scripts/string.js
@@ -1,20 +1,23 @@
-var text = 'O rato roeu a roupa do rei de roma'
-var palavras = text.split('')
-for(let i = 0; i< palavras.length; i++){
-    //alert(palavras[i])
-    if("rato" === palavras[i]){
-        palavras[i] = 'cachorro'
-    }
-}
-//alert(palavras.join(' ').replace('roma', 'grécia'))
 
-// Faça um script que conte quantas vezes a 
-// letra 'o' (minúscula) aparece na frase.
-var soma = 0
-for(let i = 0; i < palavras.length;i++){
-    if("o" === palavras[i]){
-        soma++
+function Stringg() {
+    var text = 'O rato roeu a roupa do rei de roma'
+    var palavras = text.split('')
+    for (let i = 0; i < palavras.length; i++) {
+        //alert(palavras[i])
+        if ("rato" === palavras[i]) {
+            palavras[i] = 'cachorro'
+        }
     }
+    //alert(palavras.join(' ').replace('roma', 'grécia'))
+
+    // Faça um script que conte quantas vezes a 
+    // letra 'o' (minúscula) aparece na frase.
+    var soma = 0
+    for (let i = 0; i < palavras.length; i++) {
+        if ("o" === palavras[i]) {
+            soma++
+        }
+    }
+    alert(soma)
+    alert(text)
 }
-alert(soma)
-alert(text)
