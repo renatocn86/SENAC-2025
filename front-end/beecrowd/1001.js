cd1aa2658cf942e45e8f95b5e0a4d7cc8504e77c
Renato Nascimento <96842858+renatocn86@users.noreply.github.com>
2025-07-15 22:09:37 -0300

    Questões beecrowd

---
+++ front-end/beecrowd/1001.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/Users/Aluno/Documents/GitHub/SENAC-2025/front-end/beecrowd/stdin', 'utf8');
+var lines = input.split('\n');//lines é um array
+//=========================================================
+
+let a = parseInt(lines[0])
+let b = parseInt(lines[1])
+let x = a+b
+console.log("X = "+x)
