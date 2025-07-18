f25d1c7f89b17ad0390db4bf7bcf952959cadaff
renatocn86 <96842858+renatocn86@users.noreply.github.com>
2025-07-18 19:55:54 -0300

    Update sandbox

--- front-end/pages/javascript/sandbox.js
+++ front-end/pages/javascript/sandbox.js
@@ -1,15 +1,46 @@
 
 const container = document.querySelector(".container")
 
-var panelFrag = document.createDocumentFragment()
-var div = document.createElement('div')
-div.className = 'game'
+// #region Fading squares - 200
+
+const panelFadingSquaresFrag = document.createDocumentFragment()
+const divFS = document.createElement('div')
+divFS.classList.add('component-container', 'fading-squares')
 
 for(let i = 0; i<200; i++){
+
     let sqr = document.createElement('div')
-    sqr.className = 'sqr'
-    div.appendChild(sqr)
+    sqr.className = 'sqr1'
+    divFS.appendChild(sqr)
+
 }
 
-panelFrag.appendChild(div)
-container.appendChild(panelFrag)
+panelFadingSquaresFrag.appendChild(divFS)
+
+// #endregion
+
+// #region Buttons
+
+const panelButtons = document.createDocumentFragment()
+const divB = document.createElement('div')
+divB.classList.add('component-container', 'component-container-flex')
+
+const btnRadius = document.createElement('button')
+btnRadius.className = 'btn-radius'
+btnRadius.textContent = 'ROUND CORNERS'
+
+const btnScale = document.createElement('button')
+btnScale.className = 'btn-scale'
+btnScale.textContent = 'SCALE'
+
+const btnSkew = document.createElement('a')
+btnSkew.className  = 'btn-skew'
+btnSkew.textContent = "SKEW BUTTON"
+
+divB.append(btnRadius, btnScale, btnSkew)
+panelButtons.appendChild(divB)
+
+// #endregion
+
+
+container.append(panelFadingSquaresFrag, panelButtons)
